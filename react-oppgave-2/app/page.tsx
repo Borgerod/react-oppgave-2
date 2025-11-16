4b621287e62ebe917b5dad778ea1d44b64456b71
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-11-16 20:35:00 +0100

    fix page and layout

--- react-oppgave-2/app/page.tsx
+++ react-oppgave-2/app/page.tsx
@@ -1,23 +1,80 @@
 "use client";
 import CookieClicker from "@/components/CookieClicker";
 import CallToAction from "@/components/CallToAction";
-import { useState } from "react";
+import { cn } from "./lib/utils";
+import { useRouter } from "next/navigation"; // added
 
 export default function Home() {
-	const [callToActionVisible, setCallToActionVisible] = useState(false);
+	const router = useRouter(); // added
 
 	return (
-		<div className="flex min-h-screen items-center justify-center p-30 ">
-			<main className="flex min-h-screen w-full flex-col items-start justify-start  ">
-				<CookieClicker
-					isVisible={callToActionVisible}
-					setCallToActionVisible={setCallToActionVisible}
-				/>
+		<main
+			className={cn(
+				"flex flex-col",
+				"h-screen w-screen",
+				"items-end",
+				"justify-start",
+				"items-center",
+				"content-center",
+				"py-20",
+				"px-55",
+				""
+			)}>
+			<div className={cn("flex flex-col py-30")}>
+				<h1 className={cn("text-4xl text-(--forground) mb-2", "")}>
+					React Oppgave 2
+				</h1>
+				<h2 className={cn("text-2xl text-(--secondary)", "")}>
+					Aleksander 102025
+				</h2>
+			</div>
 
-				{callToActionVisible && (
-					<CallToAction visible={callToActionVisible} />
-				)}
-			</main>
-		</div>
+			<div className="flex w-full justify-around">
+				{/* <button>
+					{" "}
+					<a href="http://localhost:3000/pages/cookie-clicker"></a>
+				</button> */}
+				<button
+					className={cn(
+						"h-10 w-50",
+						"text-background",
+						"border-none",
+						"rounded-full",
+						"bg-(--accent-light)",
+						"hover:bg-(--accent)",
+
+						""
+					)}
+					onClick={() => router.push("/pages/cookie-clicker")}>
+					Cookie Clicker
+				</button>
+				<button
+					className={cn(
+						"h-10 w-50",
+						"text-background",
+						"border-none",
+						"rounded-full",
+						"bg-(--accent-light)",
+						"hover:bg-(--accent)",
+						""
+					)}
+					onClick={() => router.push("/pages/cat-facts")}>
+					Cat Facts
+				</button>
+				<button
+					className={cn(
+						"h-10 w-50",
+						"text-background",
+						"border-none",
+						"rounded-full",
+						"bg-(--accent-light)",
+						"hover:bg-(--accent)",
+						""
+					)}
+					onClick={() => router.push("/pages/users")}>
+					Users
+				</button>
+			</div>
+		</main>
 	);
 }
